48995a2ab5f6041a68d0ef7c380a971f7a566341
Chenbo Feng <fengc@google.com>
2017-04-05 19:00:55 -0700

    BACKPORT [FROMLIST] New getsockopt option to get socket cookie
    Cherry-pick from linux net-next branch commit
    http://patchwork.ozlabs.org/patch/747590/
    
    Introduce a new getsockopt operation to retrieve the socket cookie
    for a specific socket based on the socket fd.  It returns a unique
    non-decreasing cookie for each socket.
    Tested: https://android-review.googlesource.com/#/c/358163/
    
    Test: Unit test added in kernel/tests
    
    Acked-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: Chenbo Feng <fengc@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Change-Id: I5a8af56ce7e83c51f412daf20ffc8565f877a455

--- net/core/sock.c
+++ net/core/sock.c
@@ -1181,6 +1181,7 @@ int sock_getsockopt(struct socket *sock, int level, int optname,
 
 	union {
 		int val;
+		u64 val64;
 		struct linger ling;
 		struct timeval tm;
 	} v;
@@ -1411,6 +1412,13 @@ int sock_getsockopt(struct socket *sock, int level, int optname,
 		v.val = sk->sk_incoming_cpu;
 		break;
 
+
+	case SO_COOKIE:
+		lv = sizeof(u64);
+		if (len < lv)
+			return -EINVAL;
+		v.val64 = sock_gen_cookie(sk);
+		break;
 	default:
 		/* We implement the SO_SNDLOWAT etc to not be settable
 		 * (1003.1g 7).
