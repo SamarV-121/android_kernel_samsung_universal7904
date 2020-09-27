f95ffcdd1638612a0026121a72b0ff1bbce450ef
Denis Efremov <efremov@linux.com>
2020-09-28 01:11:20 +0300

    tzdev: fix startup.tzar inclusion
    Signed-off-by: Denis Efremov <efremov@linux.com>
    Signed-off-by: K9100ii <k9100ii.ki@gmail.com>

--- drivers/misc/tzdev/3.0/tz_deploy_tzar.c
+++ drivers/misc/tzdev/3.0/tz_deploy_tzar.c
@@ -32,7 +32,7 @@
 __asm__ (
   ".section .init.data,\"aw\"\n"
   "tzdev_tzar_begin:\n"
-  ".incbin \"" KBUILD_SRC "/drivers/misc/tzdev/3.0/startup.tzar\"\n"
+  ".incbin \"startup.tzar\"\n"
   "tzdev_tzar_end:\n"
   ".previous\n"
 );
