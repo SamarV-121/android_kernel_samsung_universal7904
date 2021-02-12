ab5d6bd8bd1edb923d398483386639e06b937217
Danny Lin <danny@kdrag0n.dev>
2021-02-12 11:00:02 -0500

    f2fs: Add support for reporting a fake kernel version to fsck
    fsck.f2fs forces a filesystem fix on boot if it detects that the current
    kernel version differs from the one saved in the superblock, which results in
    fsck blocking boot for a long time (~35 seconds). This commit provides a
    way to report a constant fake kernel version to fsck to avoid triggering
    the version check, which is useful if you boot new kernel builds
    frequently.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Change-Id: I35fe42b446374b12216df5ca9e2c9f3e255a55fa
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1266,6 +1266,26 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
+static int override_version(struct new_utsname __user *name)
+{
+	int ret = 0;
+
+#ifdef CONFIG_F2FS_REPORT_FAKE_KERNEL_VERSION
+	if (strcmp(current->comm, "fsck.f2fs"))
+		return 0;
+
+	ret = copy_to_user(name->release, CONFIG_F2FS_FAKE_KERNEL_RELEASE,
+			   strlen(CONFIG_F2FS_FAKE_KERNEL_RELEASE) + 1);
+	if (ret)
+		return ret;
+
+	ret = copy_to_user(name->version, CONFIG_F2FS_FAKE_KERNEL_VERSION,
+			   strlen(CONFIG_F2FS_FAKE_KERNEL_VERSION) + 1);
+#endif
+
+	return ret;
+}
+
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
@@ -1279,6 +1299,8 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	if (override_release(name->release, sizeof(name->release)))
 		return -EFAULT;
 	if (override_architecture(name))
+		return -EFAULT;
+	if (override_version(name))
 		return -EFAULT;
 	return 0;
 }
