28701b015a2a4955ef4eb696c82ac0b27641512f
SamarV-121 <samarvispute121@pm.me>
2023-05-03 20:46:29 +0530

    Merge A405FNXXS4CWB5
    Change-Id: I50399ecfb64d74d3eb5fbab316aa2fd966cc53c9

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,7 +4,6 @@
 export PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION=r
 export ARCH=arm64
-export PROJECT_NAME=a30
 
-make ARCH=arm64 a30_swa_open_defconfig
+make ARCH=arm64 exynos7885-a40_defconfig
 make ARCH=arm64 -j64
